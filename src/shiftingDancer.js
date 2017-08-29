8a0d582c4213547390a213b325139cb69a78caf7
Hack Reactor Student <student@hackreactor.com>
2017-08-29 10:41:25 -0700

    shifting dancer added

---
+++ src/shiftingDancer.js
@@ -0,0 +1,27 @@
+
+var makeShiftingDancer = function(top, left, timeBetweenSteps) {
+
+  makeDancer.call(this, top, left, timeBetweenSteps);
+  this.step();
+
+};
+
+makeShiftingDancer.prototype = Object.create(makeDancer.prototype);
+makeShiftingDancer.prototype.constructor = makeShiftingDancer;
+
+makeShiftingDancer.prototype.step = function() {
+
+  // call the old version of step at the beginning of any call to this new version of step
+  makeDancer.prototype.step.call(this);
+
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  this.$node.animate({
+    'left' : '+=30px'
+  });
+  this.$node.animate({
+    'left' : '-=30px'
+  });
+
+};
